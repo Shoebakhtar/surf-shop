ae1604eba1dd4f2eade7bd80078d1915b47e6823
Shoebakhtar <34814131+Shoebakhtar@users.noreply.github.com>
2020-11-29 11:54:15 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto');
 const cloudinary = require('cloudinary');
 cloudinary.config({
-    cloud_name: 'dkzgk4wj0',
-    api_key: '158975334788859',
+    cloud_name: '',
+    api_key: '',
     api_secret: process.env.CLOUDINARY_SECRET
 })
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
